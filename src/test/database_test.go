a37e42cc931567a1db45f9b34d173ffb99cb1953
Eleott-hi <laptev18vv@gmail.com>
2024-05-26 23:17:50 +0700

    fix: tests

--- src/test/database_test.go
+++ src/test/database_test.go
@@ -1,4 +1,4 @@
-package test_database
+package database
 
 import (
 	"team01/database"
@@ -62,6 +62,31 @@ func TestDatabaseGet(t *testing.T) {
 	}
 }
 
+func TestDatabaseGetInvalidKey(t *testing.T) {
+	db := database.New()
+
+	key_uuid, _ := uuid.NewUUID()
+	key := database.Key(key_uuid)
+	value := database.Value{
+		"key": "value",
+	}
+
+	db.Set(key, value)
+
+	invalid_key_uuid, _ := uuid.NewUUID()
+	invalid_key := database.Key(invalid_key_uuid)
+
+	_, err := db.Get(invalid_key)
+	if err == nil {
+		t.Error(err)
+	}
+
+	if err.Error() != "key not found" {
+		t.Errorf("Expected 'key not found', got %s", err.Error())
+	}
+
+}
+
 func TestDatabaseDelete(t *testing.T) {
 	db := database.New()
 
@@ -78,5 +103,32 @@ func TestDatabaseDelete(t *testing.T) {
 
 	if len := db.Len(); len != 0 {
 		t.Errorf("Expected 0, got %d", len)
+	}
+}
+
+func TestDatabaseDeleteInvalidKey(t *testing.T) {
+	db := database.New()
+
+	key_uuid, _ := uuid.NewUUID()
+	key := database.Key(key_uuid)
+	value := database.Value{
+		"key": "value",
+	}
+
+	db.Set(key, value)
+	if err := db.Delete(key); err != nil {
+		t.Error(err)
+	}
+
+	invalid_key_uuid, _ := uuid.NewUUID()
+	invalid_key := database.Key(invalid_key_uuid)
+	
+	err := db.Delete(invalid_key)
+	if err == nil {
+		t.Error(err)
+	}
+
+	if err.Error() != "key not found" {
+		t.Errorf("Expected 'key not found', got %s", err.Error())
 	}
 }
